ae879158f2a1b0528c7043a246f1ecaa28119dac
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-30 09:34:32 +0530

    Building the App Layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  background-color: var(--color-grey-0);
+  padding: 1.2rem 4.8rem;
+`;
+
+function Header() {
+  return <StyledHeader>Header</StyledHeader>;
+}
+
+export default Header;
