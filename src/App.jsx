6ec677ae4dcd350241370dcf31b75379687a0db0
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-30 08:09:35 +0530

    Global Styles With Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+
+import Button from './ui/Button';
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Button onClick={() => alert('check in')}>Check In</Button>
+        <Button onClick={() => alert('check out')}>Check Out</Button>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
