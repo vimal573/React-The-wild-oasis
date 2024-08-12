0be454bdef66f0e5170186c994c374e2bdbfc382
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-08-12 17:27:04 +0530

    Building The App Header

---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import Logout from '../features/authentication/Logout';
+import ButtonIcon from './ButtonIcon';
+import { HiOutlineUser } from 'react-icons/hi2';
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+`;
+
+function HeaderMenu() {
+  const navigate = useNavigate();
+
+  return (
+    <StyledHeaderMenu onClick={() => navigate('/account')}>
+      <li>
+        <ButtonIcon>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <Logout />
+      </li>
+    </StyledHeaderMenu>
+  );
+}
+
+export default HeaderMenu;
